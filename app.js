529ec825d5f34169bf00043b117a75890fb57d4e
Udit <uditschoolboy@gmail.com>
2021-09-15 23:45:06 +0530

    Add cookies for signin/signup

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ async function main() {
 }
 app.use(methodOverride('_method'));
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser("secret"));
 app.use(express.urlencoded({extended:false}));
 app.use('/api', apiRoutes);
 app.use('/auth', authRoutes);
--- routes/authroutes.js
+++ routes/authroutes.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/UserModel');
 const colors = require('colors');
+const { signedCookie } = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 
 router.post('/signup', async (req, res) => {
     console.log("Sign up details : ", req.body);
     const user = new User({...req.body});
     try {
         await user.save();
+        sendCookie(user.email, req, res);
         res.send('Okay sign up complete');
     } catch(err) {
         console.log("An error".red.bgWhite);
@@ -15,13 +18,23 @@ router.post('/signup', async (req, res) => {
     }
     //send a signed cookie
 });
-router.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
     console.log("Sign in details : ", req.body);
-    const users = User.find({...req.body});
-    if(users) {
+    const user = await User.findOne({...req.body});
+    if(user) {
+        sendCookie(user.email, req, res);
         res.send('Okay login complete');
+    } else {
+        res.send('No');
     }
     //send a signed cookie
 });
-
+function sendCookie(email, req, res) {
+    const options = {
+        maxAge : 1000 * 60 * 60 * 24,
+        httpOnly : true,
+        signed : true
+    }
+    res.cookie('signed-in', email, options);
+}
 module.exports = router;
